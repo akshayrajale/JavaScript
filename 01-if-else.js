314c2fbba8021445b6375cbd3dbb0dd8cdbbd2c9
akshay rajale <akshay.rajale93@gmail.com>
2020-08-11 02:57:59 -1200

    if else stat

--- 01-if-else.js
+++ 01-if-else.js
@@ -1,5 +1,32 @@
-if(true){
-    console.log("hey");
-} else{
-    console.log("FALSE")
+let marks=35;
+if(marks>90){
+    console.log('grade1');
+}else if(marks>70){
+    console.log('grade2');
+}else if(marks>40){
+    console.log('grade3');
+}else{
+    console.log('fail');
 }
+
+
+let bal
+let wid
+let deposit
+
+let withdrow = function(a, b,c){
+     if(a>b && (a-b) >= 1000) {
+         console.log(`balance: ${a-b}`)
+     } else {
+         console.log("low balance")
+
+     }
+
+    if(b%100 == 0) {
+        console.log( b/200);
+    }else{
+    console.log("invalid")
+    }
+}
+withdrow(50000, 5000)
+
