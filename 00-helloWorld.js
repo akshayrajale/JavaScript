e5a017996c81e6b9bc9469b00af9bb58d7a74f75
akshay rajale <akshay.rajale93@gmail.com>
2020-08-11 02:56:15 -1200

    helloworld

--- 00-helloWorld.js
+++ 00-helloWorld.js
@@ -1,1 +1,24 @@
 console.log("Hello world");
+console.log('hello world');
+
+let marks=90 ;
+let totalMarks=100;
+
+let total= (90/100)*50;
+
+console.log(total);
+
+let firstName='prajakta';
+let lastName='gawali';
+
+console.log(firstName+' '+lastName);
+
+let faranheit=100;
+let celsius=(100-32)*5/9;
+
+console.log(celsius);
+
+let actualMarks=110;
+let myGrade=(actualMarks<100);
+
+console.log(myGrade);
