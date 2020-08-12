88759d306c76a395f30692ed682999e505baf89d
akshay rajale <akshay.rajale93@gmail.com>
2020-08-12 01:47:31 -1200

    atm statment

---
+++ 03-atm-statment.js
@@ -0,0 +1,43 @@
+let bal
+let wid
+
+
+let withdrow = function(a, b){
+     if(a>b && (a-b) >= 1000 && b%100 == 0) {
+
+        let twoThousand = Math.floor(b/2000);
+        console.log(`Two Thousand : ${twoThousand}`)
+        let temp = b%2000;
+
+        let oneThousand= Math.floor(temp/1000);
+        console.log(`One Thousand ${oneThousand}`);
+
+        temp = temp%1000;
+
+        let fiveHundred= Math.floor(temp/500)
+        console.log(`Five Hundred ${fiveHundred}`)
+
+        temp= temp%500;
+
+        let twoHundred= Math.floor(temp/200)
+        console.log(`Two hundred ${twoHundred}`)
+
+        temp= temp%200;
+
+
+        let oneHundred=Math.floor(temp/100)
+        console.log(`One hundred ${oneHundred}`);
+
+        temp= temp%100;
+        console.log(`Your available balance is : ${a-b}`)
+
+
+    } else {
+         console.log("Invalid")
+     }
+}
+
+withdrow(50000, 43000)
+withdrow(0, 43000)
+withdrow(123030, 200)
+withdrow(1200, 200)
