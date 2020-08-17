91ee29426b64a1bd10f67e4d926a6ac24eaeb00d
akshay rajale <akshay.rajale93@gmail.com>
2020-08-16 19:26:25 -1200

    for loop

---
+++ 06-for-loop.js
@@ -0,0 +1,42 @@
+let days= ['monday','tuesday','wendnesday','thursday','friday','saturday','sunday']
+
+for (i=0 ; i<days.length; i++){
+     console.log(days[i]);
+}
+
+
+ for (i=1 ; i<=20; i++){
+
+     console.log([i]);
+ } 
+
+ for (i=0 ; i<=10; i++){
+ 
+    if(i%2){
+        console.log([i]);
+   }
+ }
+      
+ for (i=0 ; i<=10; i++){
+ 
+    if(i%3){
+        console.log([i]);
+   }
+ }
+ 
+for (i=0 ; i<=10; i++){
+   if(i>3){
+       break;
+   }
+   console.log([i]);
+   
+ }
+
+ for (i=0 ; i<=10; i++){
+  if(i<3){
+      continue
+  }
+  console.log([i]);
+  
+}
+
