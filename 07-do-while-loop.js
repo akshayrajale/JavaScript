08c54229b203b12d0da27d41d5298d5462ea2ff1
akshay rajale <akshay.rajale93@gmail.com>
2020-08-17 19:08:56 -1200

    do while loop

---
+++ 07-do-while-loop.js
@@ -0,0 +1,14 @@
+let days= ['monday','tuesday','wendnesday','thursday','friday','saturday','sunday']
+
+let i=0;
+
+do{
+    console.log(days[i]);
+    i++;
+}while(i<days.length)
+
+i=0;
+do{
+    console.log([i]);
+    i++;
+}while(i<=10)
