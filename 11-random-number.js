7b1ac538d708a2dd2505e6af2a9c06e75edb8734
akshay rajale <akshay.rajale93@gmail.com>
2020-08-25 01:43:41 -1200

    random number

---
+++ 11-random-number.js
@@ -0,0 +1,21 @@
+let num1=22
+let num2=7
+
+result=(22/7)
+
+console.log(result)
+
+let frog= result.toFixed(2)
+
+console.log(frog)
+
+let rest = result.toFixed(2)
+console.log(Math.floor(rest));
+
+
+console.log(Math.ceil(rest));
+
+let upper =25
+let lower =21
+
+console.log(Math.random()* (upper - lower + 2)) + lower
