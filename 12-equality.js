324c35491ad7c8d74b0e7a2ad1050dbc643525f9
akshay rajale <akshay.rajale93@gmail.com>
2020-08-26 15:13:46 -1200

    equality

---
+++ 12-equality.js
@@ -0,0 +1,16 @@
+let fast= {}
+let slow= {}
+
+console.log( fast===slow)
+
+console.log(''=='')
+
+console.log(''==='')
+
+console.log(2==2)
+console.log(2===2)
+let myVar;
+console.log(myVar==myVar)
+console.log(myVar===myVar)
+
+console.log({}==myVar)
