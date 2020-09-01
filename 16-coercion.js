b435450e352bd82d0362330d7c7264ed6b543111
akshay rajale <akshay.rajale93@gmail.com>
2020-08-31 21:09:09 -1200

    coercion

---
+++ 16-coercion.js
@@ -0,0 +1,39 @@
+console.log('5'+5)
+
+console.log('5'-5)
+
+console.log( 5 - false)
+
+console.log( 5 - true)
+
+const giveType = typeof 5
+
+console.log(giveType)
+
+
+
+let loginDetails = ' '
+
+let loginId = loginDetails 
+
+if(loginId){
+    console.log('allow user to login')
+}else {
+    console.log('failed')
+}
+
+
+
+// total false below
+
+// ''
+// null
+// undefined
+// 0
+// false
+
+//total true below
+
+// {}
+// []
+// ' '
